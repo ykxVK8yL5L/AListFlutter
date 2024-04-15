d7c8c3656745757f405bc0013836b686c2e4bafd
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2024-04-16 07:07:23 +0800

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# curl -L https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
-# tar -zxvf dist.tar.gz
-# rm -rf ../public/dist
-# mv -f dist ../public
-# rm -rf dist.tar.gz
+curl -L https://github.com/ykxVK8yL5L/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+tar -zxvf dist.tar.gz
+rm -rf ../public/dist
+mv -f dist ../public
+rm -rf dist.tar.gz
